a10d12da3b323af6bfab184481a665268840dbe3
Aleksanteri <aleksanteri.heliovaara@siili.com>
2018-06-08 13:35:57 +0300

    firebase added

--- src/App.js
+++ src/App.js
@@ -2,33 +2,70 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/css/bootstrap.min.css';
 import "./styles/custom.css";
+import firebase from './firebase'
+
+
 import Navigation from './components/Navigation';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItem: '',
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this); // <-- add this line
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
   render() {
+
     return (
       <div>
-      <Navigation />
-      <div className="container">
-        <h1>Wine Lottery App</h1>
-        <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Participant Name</th>
-      <th scope="col">Delete name from the list</th>
+        <Navigation />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+          <input type="text" name="currentItem" placeholder="What are you bringing ?" onChange={this.handleChange} value={this.state.currentItem} />
+          <button>Add Item</button>
+        </form>
+        <div className="container">
+          <h1>Wine Lottery App</h1>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Participant Name</th>
+                <th scope="col">Delete name from the list</th>
 
-    </tr>
-  </thead>
-  <tbody>
-   
-    <tr>
-      <th scope="row">Matti Meikäläinen</th>
-      <td><button className="btn btn-warning btn-sm">Delete name</button></td>
+              </tr>
+            </thead>
+            <tbody>
 
-      </tr>
-  </tbody>
-</table>
-      </div>
+              <tr>
+                <th scope="row">Matti Meikäläinen</th>
+                <td><button className="btn btn-warning btn-sm">Delete name</button></td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+
+
+import firebase from 'firebase'
+const config = {
+      apiKey: "AIzaSyDgVi8uhrEZn1ySNZ6YQCYI0Lj0XzIvCcc",
+      authDomain: "crested-medium-106310.firebaseapp.com",
+      databaseURL: "https://crested-medium-106310.firebaseio.com",
+      projectId: "crested-medium-106310",
+      storageBucket: "crested-medium-106310.appspot.com",
+      messagingSenderId: "667882134601"
+};
+firebase.initializeApp(config);
+export default firebase;
